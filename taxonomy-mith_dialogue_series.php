8020f63fccbd42d5e3e7d76b33f29e3de4930d5d
kkeister <kirsten.e.keister@gmail.com>
2015-12-21 10:01:06 -0500

    update layout of dialogue series page

--- taxonomy-mith_dialogue_series.php
+++ taxonomy-mith_dialogue_series.php
@@ -1,51 +1,13 @@
 <?php get_header(); ?>
-	<?php
-	$sidebar_exists = true;
-	$sidebar_left = '1';
-	$double_sidebars = false;
-	$content_class = '';
-	
-	$sidebar_css = 'float:left;';
-	$content_css = 'float:right;';
-	
-	$page = get_page_by_title( 'Current Schedule', OBJECT, 'page' );
-	$page_id = $page->ID; 
-?>
 
-	<div id="content" class="<?php echo $content_class; ?>" style="<?php echo $content_css; ?>">
+	<div id="content" class="" style="float:right;">
         <div class="post-content">
             <?php //the_content(); ?>
             <?php get_template_part( 'templates/dialogue', 'series' ); ?>
-            <?php avada_link_pages(); ?>
         </div>
-    </div>
-
-	<div id="sidebar" class="sidebar <?php echo $sidebar_class; ?>" style="<?php echo $sidebar_css; ?>">
-		<?php
-		if( $sidebar_exists == true ) {
-			if($sidebar_left == 1) {
-				echo mith_display_sidenav( $page_id );
-				generated_dynamic_sidebar($sidebar_1);
-			}
-			if($sidebar_left == 2) {
-				generated_dynamic_sidebar_2($sidebar_2);
-			}
-		}
-		?>
 	</div>
-	<?php if( $sidebar_exists && $double_sidebars ): ?>
-	<div id="sidebar-2" class="sidebar <?php echo $sidebar_class; ?>" style="<?php echo $sidebar_2_css; ?>">
-		<?php
-		if($sidebar_left == 1) {
-			generated_dynamic_sidebar_2($sidebar_2);
-		}
-		if($sidebar_left == 2) {
-			echo avada_display_sidenav( $page_id );
-			generated_dynamic_sidebar($sidebar_1);
-		}
-		?>
-	</div>
-	<?php endif; ?>
-<?php get_footer();
-
-// Omit closing PHP tag to avoid "Headers already sent" issues.
+	<?php wp_reset_query(); ?>
+	
+	<?php get_template_part('templates/sidebar', 'dialogues');  ?>
+    
+<?php get_footer(); ?>
