6fd715050940b6400099fca8fb694618f3ae18a7
kkeister <kirsten.e.keister@gmail.com>
2015-12-21 09:59:50 -0500

    update layout of MRE page

--- archive-mith_research.php
+++ archive-mith_research.php
@@ -2,15 +2,8 @@
 
 /* Template Name: MRE */
 
-get_header();
-$content_css = 'width:100%';
-$sidebar_css = 'display:none';
-$content_class = '';
-$sidebar_exists = false;
-$sidebar_left = '';
-$double_sidebars = false;
-?>
-	<div id="content" class="fusion-portfolio fusion-portfolio-two research-portfolio" style="<?php echo $content_css; ?>">
+get_header(); ?>
+	<div id="content" class="fusion-portfolio fusion-portfolio-two research-portfolio" style="float:right;">
 	<?php 
 	
 	$sort_by = '<form method="post" id="gform_1" class="start-year-sort">
@@ -51,21 +44,9 @@ $double_sidebars = false;
 	echo do_shortcode('[one_fourth spacing="yes" last="no" id="mre_filters" class="filters-column gform_wrapper"]' . $filter_by . '[/one_fourth]');
 	echo do_shortcode('[three_fourth last="yes" spacing="yes" class="fusion-portfolio-wrapper clearfix" id="mre_items"]<!--RESEARCH ITEMS HERE-->[/three_fourth]');
 	?>
-		<?php wp_reset_query(); ?>
 	</div>
-	<?php if( $sidebar_exists == true ): ?>
 	<?php wp_reset_query(); ?>
-	<div id="sidebar" class="sidebar" style="<?php echo $sidebar_css; ?>">
-		<?php
-		if($sidebar_left == 1) {
-			generated_dynamic_sidebar($sidebar_1);
-		}
-		if($sidebar_left == 2) {
-			generated_dynamic_sidebar_2($sidebar_2);
-		}
-		?>
-	</div>
-	<?php endif; ?>
-<?php get_footer();
-
-// Omit closing PHP tag to avoid "Headers already sent" issues.
+	
+	<?php // get_template_part('templates/sidebar', 'research-items');  ?>
+    
+<?php get_footer(); ?>
